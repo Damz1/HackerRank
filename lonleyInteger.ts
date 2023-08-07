c85696e5b08705cd47e86b55ddbb9148346c90f8
David Maizil <davidmaizil@Davids-MacBook-Pro.local>
2023-08-07 09:45:08 +0100

    lonleyInteger passing the first test

--- lonleyInteger.ts
+++ lonleyInteger.ts
@@ -1,3 +1,14 @@
 // Given an array of integers, where all elements but one occur twice, find the unique element.
 
-export const lonleyInteger = (arr: []): number => {};
+export const lonleyInteger = (arr: number[]): number => {
+  const countMap: { [key: number]: number } = {};
+  for (let num of arr) {
+    countMap[num] = (countMap[num] || 0) + 1;
+  }
+  for (const num of arr) {
+    if (countMap[num] === 1) {
+      return num;
+    }
+  }
+  return 0;
+};
